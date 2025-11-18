24ec3541be74864ae38b8f1e03fa3a0418296a43
yjwu-leadstec <eugene@leads-technologies.com>
2025-11-18 18:08:25 +0800

    Add js and css into quote block

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,7 +1,11 @@
 export default function decorate(block) {
-    const [quoteWrapper] = block.children;
+  const [quoteWrapper] = block.children;
 
-    const blockquote = document.createElement('blockquote');
-    blockquote.textContent = quoteWrapper.textContent.trim();
-    quoteWrapper.replaceChildren(blockquote);
+  const blockquote = document.createElement('blockquote');
+  blockquote.textContent = quoteWrapper.textContent.trim();
+  quoteWrapper.replaceChildren(blockquote);
+
+  const author = document.createElement('p');
+  author.textContent = quoteWrapper.textContent.trim();
+  quoteWrapper.appendChild(author);
 }
